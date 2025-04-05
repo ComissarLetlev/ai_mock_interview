6d7c84de8acb9169cf67603ca833bd909d7cc0e1
Sagnik Biswas <89531647+ComissarLetlev@users.noreply.github.com>
2025-04-05 21:08:27 +0530

    final

--- firebase/admin.ts
+++ firebase/admin.ts
@@ -1,40 +1,22 @@
-import {
-  initializeApp,
-  getApps,
-  cert,
-  ServiceAccount,
-} from "firebase-admin/app";
-import { getAuth } from "firebase-admin/auth";
-import { getFirestore } from "firebase-admin/firestore";
+import admin from "firebase-admin";
 
-// Import the full service account file
-import localServiceAccount from "../serviceAccount.json";
+const serviceAccount =
+  process.env.FIREBASE_PROJECT_ID &&
+  process.env.FIREBASE_CLIENT_EMAIL &&
+  process.env.FIREBASE_PRIVATE_KEY
+    ? {
+        projectId: process.env.FIREBASE_PROJECT_ID,
+        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+        privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+      }
+    : require("../serviceAccount.json"); // Only used locally
 
-function initFirebaseAdmin() {
-  const apps = getApps();
-
-  if (!apps.length) {
-    // Build service account from env, or fallback to the local JSON
-    const serviceAccount: ServiceAccount =
-      process.env.FIREBASE_PRIVATE_KEY &&
-      process.env.FIREBASE_CLIENT_EMAIL &&
-      process.env.FIREBASE_PROJECT_ID
-        ? {
-            projectId: process.env.FIREBASE_PROJECT_ID,
-            clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-            privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
-          }
-        : (localServiceAccount as ServiceAccount); // typecast to fix TS error
-
-    initializeApp({
-      credential: cert(serviceAccount),
-    });
-  }
-
-  return {
-    auth: getAuth(),
-    db: getFirestore(),
-  };
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
+  });
 }
 
-export const { auth, db } = initFirebaseAdmin();
+const db = admin.firestore();
+
+export { admin, db };
