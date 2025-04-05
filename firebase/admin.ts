cf10b679d8487a8c3268e7910e8f24cdb0ac38ed
Sagnik Biswas <89531647+ComissarLetlev@users.noreply.github.com>
2025-04-05 20:11:06 +0530

    Configure Firebase Admin SDK with service account and environment variabls

--- firebase/admin.ts
+++ firebase/admin.ts
@@ -1,22 +1,20 @@
-import { initializeApp, cert, getApps } from "firebase-admin/app";
+import { getApps, initializeApp, cert, App } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
 import { getAuth } from "firebase-admin/auth";
 
-function initFirebaseAdmin() {
-  if (!getApps().length) {
-    initializeApp({
-      credential: cert({
-        projectId: process.env.FIREBASE_PROJECT_ID,
-        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-        privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
-      }),
-    });
-  }
+let app: App;
 
-  return {
-    auth: getAuth(),
-    db: getFirestore(),
-  };
+if (!getApps().length) {
+  app = initializeApp({
+    credential: cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+    }),
+  });
 }
 
-export const { auth, db } = initFirebaseAdmin();
+const db = getFirestore();
+const auth = getAuth();
+
+export { db, auth };
