f8325f89dd368d6a9a0d7aa2d1375cb1b99fbd15
Sagnik Biswas <89531647+ComissarLetlev@users.noreply.github.com>
2025-04-05 20:57:40 +0530

    ok

--- firebase/admin.ts
+++ firebase/admin.ts
@@ -1,7 +1,7 @@
 import { initializeApp, getApps, cert } from "firebase-admin/app";
 import { getAuth } from "firebase-admin/auth";
 import { getFirestore } from "firebase-admin/firestore";
-
+import serviceAccount from "../serviceAccount.json";
 function initFirebaseAdmin() {
   const apps = getApps();
 
